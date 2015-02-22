681400bdeeb0b77697a77abaa062877a8a405618
Saso Sabotin <sasos90@gmail.com>
2015-02-22 21:15:05 +0100

    Name added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
+				<div class="title">Laravel 5 Saso</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
