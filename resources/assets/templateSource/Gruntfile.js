50d730e42e969f114371d85fcaca5d154976605f
Saso Sabotin <sasos90@gmail.com>
2015-05-23 21:28:56 +0200

    Template source configuration: Gruntfile.js, bower.json, package.json

--- resources/assets/templateSource/Gruntfile.js
+++ resources/assets/templateSource/Gruntfile.js
@@ -1,21 +1,59 @@
 module.exports = function(grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         sass: {
+            dev: {
+                options: {
+                    trace: true,
+                    sourcemap: "file",
+                    style: "expanded",
+                    lineNumbers: true
+                },
+                files: [{
+                    src: ["scss/style.scss"],
+                    dest: "../../../public/css/style.css",
+                    ext: ".css"
+                }]
+            },
             dist: {
+                options: {
+                    style: "compressed"
+                },
+                files: [{
+                    src: ["scss/style.scss"],
+                    dest: "../../../public/css/style.css",
+                    ext: ".css"
+                }]
+            }
+        },
+        uglify: {
+            build: {
                 files: {
-                    '../../../public/css/style.css' : 'style.scss'
+                    "../../../public/js/all.min.js": ["../../../public/js/all.js"]
                 }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';\n'
+            },
+            dev: {
+                src: ['js/*.js'],
+                dest: '../../../public/js/all.js'
             }
         },
         watch: {
             css: {
-                files: '**/*.scss',
-                tasks: ['sass']
+                files: ['**/*.scss'],
+                tasks: ["sass:dev"]
+            },
+            js: {
+                files: ["js/*.js"],
+                tasks: ["concat", "uglify"]
             }
         }
     });
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default',['watch']);
+    grunt.registerTask('default', ["sass:dev", "concat"]);
+    grunt.registerTask('livebuild', ["sass:dist", "concat", "uglify"]);
 }
