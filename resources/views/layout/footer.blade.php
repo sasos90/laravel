9f2b05db264881271ea2928c401dce9ba4f2a9f0
Saso Sabotin <sasos90@gmail.com>
2015-05-23 23:21:28 +0200

    Load javascript and HTML to the layout

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,1 +1,7 @@
 <p><?= Config::get("constants.SITE_VERSION"); ?></p>
+{{-- Javascript load --}}
+<?php if (env("ENVIRONMENT") === "dev") : ?>
+    <script src="js/all.js" type="text/javascript"></script>
+<?php else : ?>
+    <script src="js/all.min.js" type="text/javascript"></script>
+<?php endif; ?>
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,3 +1,7 @@
 <title><?= isset($pageAdd["title"]) ? $pageAdd["title"] : "Baller"; ?></title>
-<link href='css/style.css' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,700,300,400' rel='stylesheet' type='text/css'>
+<?php if (env("ENVIRONMENT") === "dev") : ?>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+<?php else : ?>    
+    <link href="css/style.min.css" rel="stylesheet" type="text/css">
+<?php endif; ?>
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,700,300,400" rel="stylesheet" type="text/css">
